8f01a3c16a07d244b9499b64d73374987db71f52
Corea-Turtle <78157221+Corea-Turtle@users.noreply.github.com>
2021-05-02 20:14:00 +0900

    Create 1-7.ReferenceSwap.cpp

---
+++ 1-7.ReferenceSwap.cpp
@@ -0,0 +1,28 @@
+#include"stdafx.h"
+#include<iostream>
+using namespace std;
+
+//참조 전달이므로 호출자 변수의 값을 변경할 수 있다.
+void Swap(int& a, int& b)
+{
+	int nTmp = a;
+	a = b;
+	b = nTmp;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int x = 10, y = 20;
+
+	// 참조 전달이면 두 변수의 값이 교환된다.
+	Swap(x, y);
+
+	//교환된 결과를 출력한다.
+	cout << "x: " << x << endl;
+	cout << "y: " << y << endl;
+
+	return 0;
+}
+
+
+//의도적으로 포인터를 사용하지 않기
