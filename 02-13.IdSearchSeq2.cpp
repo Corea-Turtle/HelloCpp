cdc7ce81fb1b4658bc0644089f21d1b61cbe4fab
Corea-Turtle <78157221+Corea-Turtle@users.noreply.github.com>
2021-05-09 12:19:59 +0900

    Update 02-13.IdSearchSeq2.cpp

--- 02-13.IdSearchSeq2.cpp
+++ 02-13.IdSearchSeq2.cpp
@@ -2,19 +2,19 @@
 #include<iostream>
 using namespace std;
 
-int _tmain(int argc, _TCHAR*argv[])
+int _tmain(int argc, _TCHAR* argv[])
 {
-  int nInput = 0;
-  cout<<"11 이상의 정수를 입력하세요"<<endl;
-  cin>>nInput;
-  
-  if(nInput>10)
-  {
-  cout<<nInput<<endl;   //블록 스코프 안에 nInput 식별자가 없으므로 07번쨰줄 nInput의 메모리를 불러온다.
-  }
-  
-  else
-    cout<<"Error"<<endl;
-  
-  retrun 0;
+    int nInput = 0;
+    cout << "11 이상의 정수를 입력하세요" << endl;
+    cin >> nInput;
+
+    if (nInput > 10)
+    {
+        cout << nInput << endl;   //블록 스코프 안에 nInput 식별자가 없으므로 07번쨰줄 nInput의 메모리를 불러온다.
+    }
+
+    else
+        cout << "Error" << endl;
+
+    return 0;
 }
