3ff84e8da6a6e9cbd00e3e32c1d483b26ae422d4
Corea-Turtle <78157221+Corea-Turtle@users.noreply.github.com>
2021-05-15 18:56:51 +0900

    Create 03-10.AccessCtrl.cpp

---
+++ 03-10.AccessCtrl.cpp
@@ -0,0 +1,27 @@
+#include"stdafx.h"
+#include<iostream>
+using namespace std;
+
+//제작자 코드
+
+class CMyData
+{
+  //기본 접근 제어 지시자는 'private'이다.
+  int m_nData;
+  
+ public:
+  int GetData(void) {return m_nData; }
+  void SetData(int nParam){m_nData = nParam; }
+};
+
+//사용자 코드
+int _tmain(int argc, _TCHAR*argv[])
+{
+  CMyData data;
+  data.SetData(10);
+  cout<<data.GetData()<<endl;
+  
+  return 0;
+}
+
+//20번쨰 줄에 data.m_nData = 10 는 private이므로 허용되지 않는 문법이다.
